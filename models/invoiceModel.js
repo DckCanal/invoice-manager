3263562c55946a3bd1c931139b532ff0f690315d
Marco De Canal <marco.decanal@gmail.com>
2021-04-07 16:29:45 +0200

    invoice and patient initial data modeling

--- models/invoiceModel.js
+++ models/invoiceModel.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
-const invoiceSchema = new mongoose.Schema({});
+const invoiceSchema = new mongoose.Schema({
+    dataEmissione:{
+        type:Date,
+        default: Date.now(),
+    },
+    dataIncasso:{
+        type:Date,
+        default: Date.now(),
+    },
+    valore:{
+        type:Number,
+        required:[true,'Inserire un valore per la fattura']
+    },
+    testo:{
+        type:String,
+        default: 'Trattamento massoterapico'
+    },
+    nOrdine:{
+        type:Number,
+        required: [true,'Numero d\'ordine necessario per la fattura']
+    },
+    pazienteID:{}
+});
 const Invoice = mongoose.model('Invoice', invoiceSchema);
 module.exports = Invoice;
--- models/patientModel.js
+++ models/patientModel.js
@@ -1,5 +1,75 @@
 const mongoose = require('mongoose');
 
-const patientSchema = new mongoose.Schema({});
+const patientSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: [true, 'Nome del paziente necessario'],
+        trim: true
+    },
+    cognome: {
+        type: String,
+        trim: true,
+    },
+    codiceFiscale: {
+        type: String,
+        trim:true,
+    },
+    partitaIVA:{
+        type:String,
+        trim:true,
+    },
+    citta:{
+        type:String,
+        trim:true,
+    },
+    provincia:{
+        type:String,
+        trim:true,
+    },
+    CAP:{
+        type:String,
+        trim:true,
+    },
+    via:{
+        type:String,
+        trim:true,
+    },
+    civico:{
+        type:String,
+        trim:true,
+    },
+    telefono:{
+        type:String,
+        trim:true,
+    },
+    email:{
+        type:String,
+        trim:true,
+    },
+    nascita:{
+        citta:{
+            type:String,
+            trim:true,
+        },
+        provincia:{
+            type:String,
+            trim:true,
+        },
+        data:{
+            type:Date,
+            trim:true,
+        }
+    },
+    dataCreazione:{
+        type:Date,
+        default: Date.now(),
+    },
+    dataUltimaFattura:{
+        type:Date
+    },
+    prezzoDefault:{
+        type:Number,
+    },
+});
 const Patient = mongoose.model('Patient', patientSchema);
 module.exports = Patient;
