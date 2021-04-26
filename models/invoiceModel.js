53905cf7cea6352789524276017313e135e6e2f9
Marco De Canal <marco.decanal@gmail.com>
2021-04-26 21:44:58 +0200

    embedding patient date in invoice model

--- models/invoiceModel.js
+++ models/invoiceModel.js
@@ -1,27 +1,83 @@
 const mongoose = require('mongoose');
 
 const invoiceSchema = new mongoose.Schema({
-    dataEmissione:{
-        type:Date,
-        default: Date.now(),
+  dataEmissione: {
+    type: Date,
+    default: Date.now(),
+  },
+  dataIncasso: {
+    type: Date,
+    default: Date.now(),
+  },
+  valore: {
+    type: Number,
+    required: [true, 'Inserire un valore per la fattura'],
+  },
+  testo: {
+    type: String,
+    default: 'Trattamento massoterapico',
+  },
+  nOrdine: {
+    type: Number,
+    required: [true, "Numero d'ordine necessario per la fattura"],
+  },
+  paziente: {
+    id: {
+      type: mongoose.Schema.Types.ObjectID,
+      required: [true, 'ID paziente necessario'],
     },
-    dataIncasso:{
-        type:Date,
-        default: Date.now(),
+    nome: {
+      type: String,
+      required: [true, 'Nome del paziente necessario'],
+      trim: true,
     },
-    valore:{
-        type:Number,
-        required:[true,'Inserire un valore per la fattura']
+    cognome: {
+      type: String,
+      trim: true,
     },
-    testo:{
-        type:String,
-        default: 'Trattamento massoterapico'
+    codiceFiscale: {
+      type: String,
+      trim: true,
     },
-    nOrdine:{
-        type:Number,
-        required: [true,'Numero d\'ordine necessario per la fattura']
+    partitaIVA: {
+      type: String,
+      trim: true,
     },
-    pazienteID:{}
+    citta: {
+      type: String,
+      trim: true,
+    },
+    provincia: {
+      type: String,
+      trim: true,
+    },
+    CAP: {
+      type: String,
+      trim: true,
+    },
+    via: {
+      type: String,
+      trim: true,
+    },
+    civico: {
+      type: String,
+      trim: true,
+    },
+    nascita: {
+      citta: {
+        type: String,
+        trim: true,
+      },
+      provincia: {
+        type: String,
+        trim: true,
+      },
+      data: {
+        type: Date,
+        trim: true,
+      },
+    },
+  },
 });
 const Invoice = mongoose.model('Invoice', invoiceSchema);
 module.exports = Invoice;
